7e55c05c059f019525570b13610e218cd28473e9
João Paulo <joao_paulinh@hotmail.com>
2022-01-16 16:50:21 -0300

    add language form

--- src/components/ModalSendEmail/index.tsx
+++ src/components/ModalSendEmail/index.tsx
@@ -4,6 +4,7 @@ import { CgClose } from 'react-icons/cg'
 import { FormEmail } from './styles'
 import { FormEvent, useState } from 'react'
 import { FormProviderProps } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
 
 interface PropsModal {
   modalIsOpen: boolean
@@ -12,6 +13,7 @@ interface PropsModal {
 
 export function ModalSendEmail({ modalIsOpen, closeModal }: PropsModal) {
   const [isLoading, setIsLoading] = useState(false)
+  const { t } = useTranslation()
 
   async function sendEmail(event : any) {
     setIsLoading(true)
@@ -45,9 +47,9 @@ export function ModalSendEmail({ modalIsOpen, closeModal }: PropsModal) {
       <button type="button" onClick={closeModal} className="react-modal-close">
         <CgClose className="closeModal" />
       </button>
-      <h1>Entre em contato</h1>
+      <h1>{t("Entre em contato")}</h1>
       <FormEmail onSubmit={sendEmail}>
-        <input name="name" type="text" placeholder="Nome" required />
+        <input name="name" type="text" placeholder={t("Nome")} required />
         <input
           name="email"
           type="text"
@@ -56,17 +58,17 @@ export function ModalSendEmail({ modalIsOpen, closeModal }: PropsModal) {
         />
         <textarea
           name="message"
-          placeholder="Digite a mensagem aqui"
+          placeholder={t("Digite a mensagem aqui")}
           required
         ></textarea>
         <div>
-          <input type="reset" value="Apagar" />
+          <input type="reset" value={String(t("Resetar"))} />
           {isLoading ? (
             <div className="loading">
               <div></div>
             </div>
           ):(
-            <input type="submit" value="Enviar" />
+            <input type="submit" value={String(t("Enviar"))} />
           )}
         </div>
       </FormEmail>
